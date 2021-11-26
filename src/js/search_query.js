e094d50272513278e7340f2caafde03285164ddc
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2021-11-26 23:52:16 +0200

    Revert "genre menu and search list"

--- src/js/search_query.js
+++ src/js/search_query.js
@@ -1,36 +1,11 @@
 // 1.Рассширить форму поиска
 import FilmCard from '../templates/filmCard.hbs';
-
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
-const { alert, error } = require('@pnotify/core');
-
 import getRefs from './get-refs';
 import { getGenreString, getYearString, getImages } from './fetchMoviesAPI';
-
 
 const refs = getRefs();
 
 const inputRefs = document.getElementById('search__form');
-
-
-  const value = e.currentTarget.elements.query.value.trim();
-
-  try {
-    if (value === '') return;
-    API.searchQuery = value;
-    const getFilmList = await API.searchMovies(value);
-
-    if (getFilmList.results.length === 0) {
-      return onInfo();
-    }
-
-    await renderPage(getFilmList);
-    refs.form.reset();
-  } catch (error) {
-    onError();
-  }
-
 
 const API_KEY = '0556b87ba267edab76fd3e7e8d7e5097';
 
@@ -40,27 +15,9 @@ const SEARCH_URL = `${BASE_URL}/search/movie`;
 
 refs.form.addEventListener('submit', onHandlerInput);
 
-
-async function renderPage(card) {
+function renderPage(card) {
   refs.films.innerHTML = '';
   refs.films.insertAdjacentHTML('beforeend', FilmCard(card));
-}
-
-
-function onError() {
-  return error({
-    text: 'Search result not successful. Enter the correct movie name!',
-    type: 'Error',
-    delay: 1500,
-  });
-}
-
-function onInfo() {
-  return alert({
-    text: 'Sorry, but no such movie was found.',
-    type: 'info',
-    delay: 1500,
-  });
 }
 
 async function onHandlerInput(e) {
@@ -91,5 +48,4 @@ export default function showErrorMsg() {
 function clearErrorMsg() {
   refs.errorMsg.textContent = " ";
 }
-
 
