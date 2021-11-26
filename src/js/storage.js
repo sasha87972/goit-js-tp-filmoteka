661c0d18ba4a70a2b9801184d635440d109bc609
Polosmakr <polosmakrr@gmail.com>
2021-11-26 21:52:50 +0200

    my-lib
    добавил логику на кнопку удалить

--- src/js/storage.js
+++ src/js/storage.js
@@ -5,7 +5,8 @@ const refs = getRefs();
 const watchedBtn = document.querySelector('.modalBtn__item--watchedBtn');
 const queueBtn = document.querySelector('.modalBtn__item--queueBtn');
 
-localStorage.clear();
+
+// localStorage.clear();
 
 
 let watched = JSON.parse(localStorage.getItem('filmWatched'));
@@ -17,11 +18,43 @@ if (!queue) {
   queue = [];
 }
 function onRemove() {
-  console.log('remove')
+  const queueBtnCurrent = refs.queueBtn.classList.contains('header__btn--current');
+  const watchedBtnCurrent = refs.watchedBtn.classList.contains('header__btn--current');
+
+  let currentFilm = JSON.parse(localStorage.getItem('currentFilm'));
+  let watched = JSON.parse(localStorage.getItem('filmWatched'));
+  let queue = JSON.parse(localStorage.getItem('filmQueue'));
+
+  if (queueBtnCurrent) {
+    // console.log('queue array', queue);
+    // console.log(currentFilm);
+    const id = currentFilm.id;
+    const index = queue.findIndex(obj => obj.id === id);
+    const newData = [
+    ...queue.slice(0, index),
+    ...queue.slice(index + 1)
+]
+   localStorage.setItem('filmQueue',JSON.stringify(newData))
+    // console.log(newData);
+  }
+  else if (watchedBtnCurrent) {
+    // console.log('watched array', watched);
+    // console.log(currentFilm);
+    const id = currentFilm.id;
+    const index = watched.findIndex(obj => obj.id === id);
+    const newData = [
+    ...watched.slice(0, index),
+    ...watched.slice(index + 1)
+]
+   localStorage.setItem('filmWatched',JSON.stringify(newData))
+    // console.log(newData);
+  }
 }
+
 
 function onWatchedLib() {
   console.log('click on watched');
+  onRemove();
   let currentFilm = JSON.parse(localStorage.getItem('currentFilm'));
   let watched = JSON.parse(localStorage.getItem('filmWatched'));
   if (!watched) {
@@ -35,10 +68,12 @@ function onWatchedLib() {
   }
   watched.push(currentFilm);
   localStorage.setItem('filmWatched', JSON.stringify(watched));
+  
 }
 
 function onQueueLib() {
   console.log('click on queue');
+  onRemove();
   let currentFilm = JSON.parse(localStorage.getItem('currentFilm'));
   let queue = JSON.parse(localStorage.getItem('filmQueue'));
   if (!queue) {
@@ -52,12 +87,13 @@ function onQueueLib() {
   }
   queue.push(currentFilm);
   localStorage.setItem('filmQueue', JSON.stringify(queue));
+  
 }
 function generateLib(e) {
-    return JSON.parse(localStorage.getItem(e));
+  return JSON.parse(localStorage.getItem(e));
 
   // if (e.target.id === 'watched') {
-    // console.log('target W', e.target.id);
+  // console.log('target W', e.target.id);
   //   let watchedLib = JSON.parse(localStorage.getItem(e));
   //   console.log(watchedLib);
   //   const watchedFilmLib = FilmCard(watchedLib);
@@ -74,4 +110,4 @@ function generateLib(e) {
   // }
 }
 
-export { onRemove,onWatchedLib, onQueueLib, generateLib };
+export { onRemove, onWatchedLib, onQueueLib, generateLib };
