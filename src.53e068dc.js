bb1f49e0ca2701b1c78770e33d9b03b823949d7d
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2021-11-29 22:11:00 +0000

    Deploying to gh-pages from @ sasha87972/goit-js-tp-filmoteka@bf31e9de96c212666c6f58184655c3b6cbb74f3f 🚀

--- src.53e068dc.js
+++ src.53e068dc.js
@@ -59,4 +59,4 @@ var e,n=arguments[3];!function(t){"object"==typeof exports&&"undefined"!=typeof 
 },{"./getFetch":"e0HM","./get-refs":"EGsz","../templates/genreCard.hbs":"IRcU"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./templates/filmCard.hbs"),require("./js/fetchMoviesAPI"),require("./js/footerModal"),require("./js/loader"),require("./js/pagination"),require("./js/colorSwitch"),require("./js/movieModal"),require("./js/header"),require("./js/storage"),require("./js/search_query"),require("./js/backToTop"),require("./js/getFetch"),require("./js/genre"),require("./js/genre-search");
 },{"./sass/main.scss":"clu1","./templates/filmCard.hbs":"rrA0","./js/fetchMoviesAPI":"lyak","./js/footerModal":"EagV","./js/loader":"vQE2","./js/pagination":"BWUK","./js/colorSwitch":"Kkkx","./js/movieModal":"z2q0","./js/header":"cGsR","./js/storage":"qhja","./js/search_query":"kwaO","./js/backToTop":"PQ5t","./js/getFetch":"e0HM","./js/genre":"Sl3q","./js/genre-search":"UTmX"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-tp-filmoteka/src.4f3cc0dc.js.map
+//# sourceMappingURL=/goit-js-tp-filmoteka/src.53e068dc.js.map
