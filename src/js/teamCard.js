5b29fdbfd13b3bc31068663bd54cab69fb913c8d
sasha87972 <82048515+sasha87972@users.noreply.github.com>
2021-12-06 20:13:29 +0200

    fixes

--- src/js/teamCard.js
+++ src/js/teamCard.js
@@ -3,7 +3,6 @@ export default [
     name: 'Alexandra Vdovychenko',
     photo:
       'https://raw.githubusercontent.com/sasha87972/goit-js-tp-filmoteka/main/src/images/team/aleksandra.jpg',
-
   },
   {
     name: 'Iryna Shandar',
